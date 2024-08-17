1b1a2a21b28aa67c5ffd40cb85cbb89b7dbc77c2
Gabriel Ramos <gbr.rms@outlook.com>
2024-08-17 19:06:08 -0300

    another another test again!

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://ligads.github.io/ligads', // Set the base to root
+  base: 'https://ligads.github.io/', // Set the base to root
   server: {
     host: true
   },
